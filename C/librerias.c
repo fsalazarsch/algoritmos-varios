28eb0badac39940fc667aaba53fb696395129a77
Felipe Salazar Schlotterbeck <fsalazar.sch@gmail.com>
2016-08-18 22:59:51 -0300

    Create librerias.c

---
+++ C/librerias.c
@@ -0,0 +1,16 @@
+/*
+* autor: Felipe Salazar Sch.
+* demostracion de librería estandar y libreria no estandar en C
+*/
+#include <stdio.h>
+#include <math.h>
+#include "math.h"
+ 
+int main(){
+    int j=0, i= 1;
+    printf("\n%f", pow(2,3));
+    printf("\n%f", powe(0,0));
+     
+    getchar();
+    return 0;
+    }
