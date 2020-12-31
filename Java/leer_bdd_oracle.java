8e6a6f89e41d0c8f718a4956251eccd05350d0e4
Felipe Salazar Schlotterbeck <fsalazar.sch@gmail.com>
2020-12-31 01:16:10 -0300

    Create leer_bdd_oracle.java

---
+++ Java/leer_bdd_oracle.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.sql.*;//conecta a base de datos
+class none{
+
+public static void main(String a[] ){
+
+ String dbUrl="jdbc:odbc:bddpropia";// conexion via odbc
+ String user="user";
+ String password="pass";
+
+ 
+ try{
+ 
+  Class.forName("sun.jdbc.odbc.JdbcOdbcDriver");// carga los driver de conexion
+  Connection c=DriverManager.getConnection(dbUrl,user,password);// establece conexxion se llama c
+  Statement s=c.createStatement();
+  String cadenaSql="SELECT * FROM alumno";
+  ResultSet r =s.executeQuery( cadenaSql);// el resultado del la consulta lo deja en r
+  boolean existe=false;
+  
+  while(r.next()){
+  
+  existe=true;
+  System.out.println("RUT : "+r.getString("rut")+ " --NOMBRE: "+r.getString("nombre")+ " --Apellido: "+r.getString("email"));// get obtiene los tipos de datos de la tabla
+  
+  }
+  if (!existe)
+  System.out.println("No se encontraron datos en la tabla");
+  r.close();
+  s.close();
+  c.close();
+ }catch (ClassNotFoundException ex){
+  System.out.println("error:"+ex.getMessage());
+ }catch(SQLException ex){
+  System.out.println("error:"+ex.getMessage());
+ }
+
+
+}
+
+}
