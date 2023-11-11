44c2fa41e47a2d459e66a4de92b00920c9249c02
Felipe Salazar Schlotterbeck <fsalazar.sch@gmail.com>
2023-11-10 22:18:27 -0300

    Update bar_qr_code.py

--- python/bar_qr_code.py
+++ python/bar_qr_code.py
@@ -54,6 +54,21 @@ for i in datas:
 data = "ABCDEFGH"
 code = barcode.Code128(data, writer=barcode.writer.ImageWriter())
 
+
+########################################################################
+from io import BytesIO
+
+from barcode import EAN13
+from barcode.writer import SVGWriter
+
+# Write to a file-like object:
+rv = BytesIO()
+EAN13(str("100000902922"), writer=SVGWriter()).write(rv)
+
+# Or to an actual file:
+with open("somefile.svg", "wb") as f:
+    EAN13(str(100000011111), writer=SVGWriter()).write(f)
+
 img = code.render()
 
 plt.imshow(img)
